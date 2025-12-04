dd013a4bb68705642ebab107750a604efd815482
Maxwell <maxwell.dinsmore@dcmail.ca>
2025-12-03 20:44:45 -0500

    another navigation fix

--- lib/snake_game.dart
+++ lib/snake_game.dart
@@ -10,7 +10,6 @@ import 'package:provider/provider.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'app_provider.dart';
 import 'screens/leaderboard_screen.dart';
-import 'screens/home_screen.dart';
 import '../tempdata.dart' as temp_data;
 import '../database.dart'; 
 class SnakeGame extends StatefulWidget {
@@ -535,7 +534,7 @@ class _SnakeGameState extends State<SnakeGame> {
                 // Close dialog and navigate to leaderboard.
                 Navigator.of(context).pop(); 
                 Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(builder: (_) => const LeaderboardScreen()),
+                  MaterialPageRoute(builder: (_) => const MainScreen()),
                 );
               },
               style: TextButton.styleFrom(
